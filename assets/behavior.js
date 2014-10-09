b9c07812279ce719983a51c5077375dfb40a3e94
aureooms <aurelien.ooms@gmail.com>
2014-10-09 21:36:36 +0200

    Generated documentation for 6f95b0f63b786ad77942a0c6a9aa9bd1f527de24

--- assets/behavior.js
+++ assets/behavior.js
@@ -151,6 +151,26 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
               "foldPrefix": "^",
               "name": "JavaScript"
             },
+            "sourcePath": "/home/genius/dev/js/js-operator/js/src/cast/identity.js",
+            "projectPath": "js/src/cast/identity.js",
+            "targetPath": "cast/identity",
+            "pageTitle": "cast/identity",
+            "title": "identity"
+          },
+          "depth": 2,
+          "outline": []
+        }, {
+          "type": "file",
+          "data": {
+            "language": {
+              "nameMatchers": [".js"],
+              "pygmentsLexer": "javascript",
+              "multiLineComment": ["/*", "*", "*/"],
+              "singleLineComment": ["//"],
+              "ignorePrefix": "}",
+              "foldPrefix": "^",
+              "name": "JavaScript"
+            },
             "sourcePath": "/home/genius/dev/js/js-operator/js/src/cast/truth.js",
             "projectPath": "js/src/cast/truth.js",
             "targetPath": "cast/truth",
