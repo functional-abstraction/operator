aca904602be209bc19844f4c4fedd6e606c1f71c
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2021-05-01 10:43:42 +0200

    :books: docs(len): Add function signature.

--- src/list/len.js
+++ src/list/len.js
@@ -1,1 +1,13 @@
-export const len = (obj) => obj.length;
+/**
+ * Returns the length of the input.
+ *
+ * @example
+ * // returns 7
+ * import {list} from '@iterable-iterator/list';
+ * import {range} from '@iterable-iterator/range';
+ * len( list( range( 7 ) ) ) ;
+ *
+ * @param {any} x Input.
+ * @return {Number}
+ */
+export const len = (x) => x.length;
