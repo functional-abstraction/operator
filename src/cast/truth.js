73439942cbea66a97dadfa3e69aabf5a1d155bfc
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2022-06-27 01:25:46 +0200

    :arrow_up: deps: Upgrade xo to v0.50.0.

--- src/cast/truth.js
+++ src/cast/truth.js
@@ -1,2 +1,3 @@
+// eslint-disable-next-line unicorn/prefer-native-coercion-functions
 const truth = (x) => Boolean(x);
 export default truth;
--- src/num/unary/pos.js
+++ src/num/unary/pos.js
@@ -1,2 +1,3 @@
+// eslint-disable-next-line unicorn/prefer-native-coercion-functions
 const pos = (obj) => Number(obj);
 export default pos;
