f810a26d183aa7c21391d7243b47838905d0a9a0
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2022-07-23 18:50:20 +0200

    :books: docs: Update project name and usage import sample code.

--- doc/scripts/header.js
+++ doc/scripts/header.js
@@ -10,7 +10,7 @@ const domReady = function (callback) {
 domReady(() => {
 	const projectname = document.createElement('a');
 	projectname.classList.add('project-name');
-	projectname.text = 'functional-abstraction/operator';
+	projectname.text = '@functional-abstraction/operator';
 	projectname.href = './index.html';
 
 	const header = document.querySelector('header');
